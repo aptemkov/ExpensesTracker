10124029c67686f91687bdcf0fd96dd4b94b35da
aptemkov <artkov37@gmail.com>
2022-11-20 18:55:12 +0300

    Fragment home design created.

--- app/src/main/java/com/github/aptemkov/expensestracker/MainActivity.kt
+++ app/src/main/java/com/github/aptemkov/expensestracker/MainActivity.kt
@@ -2,6 +2,8 @@ package com.github.aptemkov.expensestracker
 
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
